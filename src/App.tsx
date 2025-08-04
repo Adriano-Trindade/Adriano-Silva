3e2520535626cd65b48c75236e65b8abd844393e
Adriano silva <adrianotrindade165@gmail.com>
2025-08-04 11:30:17 +0000

    change to port

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const MyApp: FC = () => {
 
     return (
         <div className={classes.container}>
-            <h1>{i18n.t('Hello {{name}}', { name: data?.me?.name })}</h1>
+            <h1>{i18n.t('Olá {{name}}', { name: data?.me?.name })}</h1>
             <h3>{i18n.t('Benvindo ao DHIS2 com TypeScript!')}</h3>
         </div>
     )
