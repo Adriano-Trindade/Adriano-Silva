1ce831e481782d02f741fd422b545c6029272764
Adriano silva <adrianotrindade165@gmail.com>
2025-08-07 10:00:25 +0000

    New table header with Processo unico

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,20 @@ import React, { FC } from 'react'
 import classes from './App.module.css'
 import { Pagination } from "@dhis2/ui";
 import { Menu } from './componentes/Menu';
-import { Route, Routes } from 'react-router-dom';
+import { data, Route, Routes } from 'react-router-dom';
 import { AppRoutes } from './componentes/appRoutes';
 import Rodape from './componentes/rodopé';
 
 
 
 
-interface QueryResults {
-    me: {
-        name: string
-    }
-}
-
-const query = {
-    me: {
-        resource: 'me',
-    },
-}
-
 const MyApp: FC = () => {
-    const { error, loading, data } = useDataQuery<QueryResults>(query)
+    
 
-    if (error) {
-        return <span>{i18n.t('ERROR')}</span>
-    }
-
-    if (loading) {
-        return <span>{i18n.t('Loading...')}</span>
-    }
-
-    return (
-        <div className={classes.container}>
+    return (        
+       <div className={classes.container}>
+            <h1>{i18n.t('Seja Bem-vindo')}</h1>
+            
         
             <AppRoutes />
             <Rodape />
@@ -44,6 +26,9 @@ const MyApp: FC = () => {
 
 
     )
+      
 }
+   
+
 
 export default MyApp
--- src/componentes/Laboratorio.tsx
+++ src/componentes/Laboratorio.tsx
@@ -1,52 +1,102 @@
 import React from "react";
-import transferir from './imagem/transferir.jpeg';
+//import transferir from './imagem/transferir.jpeg';
 import {
+    DataTable,
+    DataTableCell,
+    DataTableColumnHeader,
+    DataTableRow,
     Table,
     TableBody,
     TableCell,
     TableCellHead,
+    TableFoot,
     TableHead,
     TableRow,
     TableRowHead,
 } from '@dhis2/ui'
+import i18n from "../locales";
+import { useDataQuery } from "@dhis2/app-runtime";
 
 
+
+
+interface QueryResults {
+    results: {
+        name: string
+        id: string
+        programTrackedEntityAttributes: {
+            trackedEntityAttribute: {
+                id: string
+                displayName: string
+                valueType: string
+                optionSet?: {
+                    options: {
+                        code: string
+                        name: string
+                    }[]
+                }
+            }
+        }[]
+    }
+}
+
+const query = {
+    results: {
+        "resource": "programs",
+        "id": "rTmLXWgtUj2",
+        "params": {
+            "fields": [
+                "id, name",
+                "programTrackedEntityAttributes[trackedEntityAttribute[id,optionSet,displayName,valueType,optionSet[options[code,name]]]]"
+
+            ]
+
+        }
+    }
+}
 
 export default function Laboratorio() {
+    const { error, loading, data } = useDataQuery<QueryResults>(query)
+
+    if (error) {
+        return <span>{i18n.t('ERROR')}</span>
+    }
+
+    if (loading) {
+        return <span>{i18n.t('Loading...')}</span>
+    }
+
     return (
-        <div className="styles.container">
-            <div className="styles.imagem">
-                <img src={transferir} alt="Transferir" />
-                <h1>Bem-vindo ao Laboratório</h1>
-            </div>
-            <div className="styles.tabela">
-                <Table>
-                    <TableHead>
-                        <TableRowHead>
-                            <TableCellHead>Nome</TableCellHead>
-                            <TableCellHead>Data</TableCellHead>
-                            <TableCellHead>Tipo de Teste</TableCellHead>
-                            <TableCellHead>Resultado</TableCellHead>
-                        </TableRowHead>
-                    </TableHead>
-                    <TableBody>
-                        {/* Aqui você pode mapear os dados do laboratório */}
-                        <TableRow>
-                            <TableCell>Exemplo de Teste</TableCell>
-                            <TableCell>01/01/2023</TableCell>
-                            <TableCell>Teste de Sangue</TableCell>
-                            <TableCell>Positivo</TableCell>
-                        </TableRow>
-                         <TableRow>
-                            <TableCell>Exemplo de Teste</TableCell>
-                            <TableCell>01/01/2025</TableCell>
-                            <TableCell>Teste de urina</TableCell>
-                            <TableCell>Negativo</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </div>
+        <div>
+            <h1 style={{ textAlign: 'center', color: 'green' }}>Bem vindo ao laboratorio</h1>
+            <h1 style={{ textAlign: 'center', color: 'blue' }}>Programa: {data.results.name}</h1>
+
+            <DataTable>
+                <TableHead>
+                    <DataTableRow>{
+                        data.results.programTrackedEntityAttributes.map((attribute) => (
+                            <DataTableColumnHeader key={attribute.trackedEntityAttribute.id}>
+                                {attribute.trackedEntityAttribute.displayName}
+                            </DataTableColumnHeader>
+                        ))
+                    }
+                    </DataTableRow>
+                </TableHead>
+                <TableBody>
+                    <DataTableRow>
+                        <DataTableCell>Onyekachukwu</DataTableCell>
+                    </DataTableRow>
+                </TableBody>
+                <TableFoot>
+                    <DataTableRow>
+                        <DataTableCell>Footer content</DataTableCell>
+                    </DataTableRow>
+                </TableFoot>
+            </DataTable>
+
+
+
+
         </div>
     )
-
 }
